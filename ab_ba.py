eca33400f82d149b641f8f02aa8d9f1333198113
Samrat Mukherjee <40349512+Samrat230599@users.noreply.github.com>
2020-07-24 16:30:39 +0530

    Create ab_ba.py

---
+++ ab_ba.py
@@ -0,0 +1,8 @@
+#Question: https://codeforces.com/problemsets/acmsguru/problem/99999/112
+
+#probably the easiest in Codeforces
+
+#language: Python
+
+a , b = map(int , input().split())
+print((a**b) - (b**a))
